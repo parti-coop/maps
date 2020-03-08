8d6a5a839637910d6fb9670321998cfb6c3ef549
Swain Hwang <swain@swainuicbookpro.lan>
2020-03-08 03:00:53 -0700

    init naver map api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+function initMap() {
+  const mapOptions = {
+    center: new window.naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 10
+  };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  const map = new window.naver.maps.Map("map", mapOptions);
+  console.log(map);
+}
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+initMap();
